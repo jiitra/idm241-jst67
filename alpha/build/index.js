04fb08e22f42902d2be93dac49006f2f74651c48
jiitra <74521649+jiitra@users.noreply.github.com>
2022-10-06 20:03:34 -0400

    fully functional!

--- alpha/build/index.js
+++ alpha/build/index.js
@@ -4,3 +4,4 @@ document.querySelector("#trigger-overlay").addEventListener("click", function ()
 document.querySelector(".overlay-close").addEventListener("click", function () {
 	document.querySelector(".overlay").classList.remove("open");
 });
+
