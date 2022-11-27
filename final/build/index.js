3d31c9a84f5327557665ce28ad3c4c216ca1c721
jiitra <74521649+jiitra@users.noreply.github.com>
2022-11-27 14:58:48 -0500

    all functionality

--- final/build/index.js
+++ final/build/index.js
@@ -3,25 +3,31 @@ document
   .addEventListener("click", function () {
     document.querySelector(".overlay").classList.add("open");
   });
+
 document.querySelector(".overlay-close").addEventListener("click", function () {
   document.querySelector(".overlay").classList.remove("open");
 });
 
-var resume = document.querySelector("#resume");
 var about = document.querySelector("#about");
+var resume = document.querySelector("#resume");
 var contact = document.querySelector("#contact");
-let color;
 
-resume.addEventListener('click', function() {
-  document.querySelector(".overlay").style.background = "red";
+about.addEventListener("click", function () {
+  about.querySelector(".underline").classList.add("active");
+  document.querySelector(".overlay").style.transition = "all 0.5s";
+  document.querySelector(".overlay").style.background = "#87ECEC";
 });
 
-about.addEventListener('click', function() {
-  document.querySelector(".overlay").style.background = "blue";
+resume.addEventListener("click", function () {
+  resume.querySelector(".underline").classList.add("active");
+  document.querySelector(".overlay").style.transition = "all 0.5s";
+  document.querySelector(".overlay").style.background = "#FDD867";
 });
 
-contact.addEventListener('click', function() {
-  document.querySelector(".overlay").style.background = "orange";
+contact.addEventListener("click", function () {
+  contact.querySelector(".underline").classList.add("active");
+  document.querySelector(".overlay").style.transition = "all 0.5s";
+  document.querySelector(".overlay").style.background = "#F8D0E5";
 });
 
 function delay(URL) {
